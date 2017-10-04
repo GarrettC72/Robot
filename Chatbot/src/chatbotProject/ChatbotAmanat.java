dd40d7bc85f421eba43c594c63356059aafaaace
emon200 <ahossain8400@bths.edu>
2017-10-04 09:53:14 -0400

    Update ChatbotAmanat.java

--- Chatbot/src/chatbotProject/ChatbotAmanat.java
+++ Chatbot/src/chatbotProject/ChatbotAmanat.java
@@ -2,6 +2,12 @@ package chatbotProject;
 
 public class ChatbotAmanat implements Topic {
 
+	private String username;
+	private boolean onSpicy;
+	private String sour;
+	private String spicy;
+	private String sweet;
+	private String bitter;
 	private String[] keywords;
 	private String goodbyeWord;
 	private String secretWord;
@@ -9,10 +15,14 @@ public class ChatbotAmanat implements Topic {
 	
 	
 	public ChatbotAmanat() {
-		String[] temp = {"food","entertainment","Internet","video games"};
-		keywords = temp;
+		String[] keywords = {"flavor","taste","savor","zest"};
+		spicy = "spicy";
+		sour = "sour;
+		sweet = "sweet";
+		bitter = "bitter";
+		this.keywords = keywords;
 		goodbyeWord = "bye";
-		secretWord = "pug";
+		secretWord = "flavor town";
 	}
 	
 	@Override
@@ -27,7 +37,8 @@ public class ChatbotAmanat implements Topic {
 
 	@Override
 	public void startChatting(String response) {
-		ChatbotMain.print("Hey! It sounds like you and I have a common interest! Let's talk some more!");
+		username = ChatbotMain.chatbot.getUsername()
+		ChatbotMain.print("Hey! It sounds like you and I have a common interest! Let's talk some more " + username + "!");
 		chatting = true;
 		while(chatting) {
 		 response = ChatbotMain.getInput();
@@ -37,6 +48,18 @@ public class ChatbotAmanat implements Topic {
 		 }
 		 else if(ChatbotMain.findKeyword(response,secretWord,0) >= 0) {
 			 ChatbotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now."); 
+		 }
+		 else if(ChatbotMain.findKeyword(response,sweet,0) >= 0) {
+				 ChatbotMain.print("Ah how I love sweets! You must love them too no? Let me pull up a list of sweets."); 
+		 }
+		 else if(ChatbotMain.findKeyword(response,sour,0) >= 0) {
+			 ChatbotMain.print("Say yes and I'll make sure to show you some foods that'll make you pucker!"); 
+		 }
+		 else if(ChatbotMain.findKeyword(response,spicy,0) >= 0) {
+			 ChatbotMain.print("Spicy foods for a spicy person! Would you like a list of torturous foods?"); 
+		 }
+		 else if(ChatbotMain.findKeyword(response,bitter,0) >= 0) {
+			 ChatbotMain.print("I see you are a connoisseur of bitter foods. Do you want a list of some?"); 
 		 }
 		 else {
 			 ChatbotMain.print("Huh. I don't really get you. Tell me something else?");
