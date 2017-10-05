90954d2af703cb1bf5aee10b6dfbfa65191ed60f
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-10-05 11:31:31 -0400

    Changed hello

--- Chatbot/src/chatbotProject/ChatbotAmanat.java
+++ Chatbot/src/chatbotProject/ChatbotAmanat.java
@@ -19,7 +19,7 @@ public class ChatbotAmanat implements Topic {
 	private String terminate;
 	private int hellosC;
 	private boolean terminated;
-	
+	s
 	
 	public ChatbotAmanat() {
 		String[] keywords = {"flavor","taste","savor","zest"};
